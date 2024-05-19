570869d727a16793c5af14d622e2016732313ab9
Alessandro Ruffo <ale.ruffo@live.it>
2024-05-19 18:59:47 +0200

    feat: added first version of hompage and usercard component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import UserCard from '../../components/UserCard';
+
+const user = [
+  {
+    name: 'John Doe',
+    username: 'johndoe',
+    profilePicture: 'https://i.pravatar.cc/',
+    location: 'New York',
+    age: 25,
+    mainInstrument: 'piano',
+  },
+  {
+    name: 'Jane Doe',
+    username: 'janedoe',
+    profilePicture: 'https://i.pravatar.cc/',
+    location: 'Los Angeles',
+    age: 22,
+    mainInstrument: 'headphones',
+  },
+  {
+    name: 'Alice Doe',
+    username: 'alicedoe',
+    profilePicture: 'https://i.pravatar.cc/',
+    location: 'Chicago',
+    age: 27,
+    mainInstrument: 'mic-external-on',
+  },
+];
 
 const Home = () => {
   return (
-    <SafeAreaView className="p-4 bg-background-default h-full">
-      <Text className="text-text text-3xl font-pbold">Feed</Text>
-    </SafeAreaView>
+    <ScrollView className="bg-background-default">
+      <SafeAreaView className="p-4 bg-background-default h-full">
+        <Text className="text-text text-3xl font-pbold">Feed</Text>
+        <View className="mt-4">
+          <UserCard user={user[0]} />
+          <UserCard user={user[1]} />
+          <UserCard user={user[2]} />
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
---
+++ components/UserCard.jsx
@@ -0,0 +1,40 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Icon, Image } from 'react-native-elements';
+import { BlurView } from 'expo-blur';
+
+const UserCard = ({ user }) => {
+  return (
+    <TouchableOpacity className="rounded-3xl bg-secondary-opacity50 mb-4">
+      <View className="relative ">
+        <Image source={{ uri: user.profilePicture }} className="w-full h-48 rounded-3xl" />
+
+        <BlurView className="absolute bottom-2 left-2 p-4 rounded-full overflow-hidden bg-background-opacity50">
+          <Text className="text-text font-psemibold">@{user.username}</Text>
+        </BlurView>
+        <TouchableOpacity className="absolute bottom-2 right-2 ">
+          <BlurView className="p-4 rounded-full overflow-hidden bg-background-opacity50">
+            <Icon name="chat-bubble" type="material" color="#F0ECF7" />
+          </BlurView>
+        </TouchableOpacity>
+      </View>
+      <View className="p-4 flex-row justify-between">
+        <View className="flex-row items-center">
+          <View className="p-1 rounded-full overflow-hidden bg-primary-default mr-2">
+            <Icon name={user.mainInstrument} type="material" color={'#F0ECF7'} />
+          </View>
+          <Text className="text-text text-xl font-pbold">
+            {user.name}, {user.age}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center ">
+          <Icon name="location-on" type="material" color="#fff" />
+          <Text className="text-text text-base font-psemibold ml-1">{user.location}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default UserCard;
